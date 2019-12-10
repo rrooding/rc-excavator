2385deefab7adbce46dab4f94b73e79d43553ab9
Ralph Rooding <ralph@ralphrooding.com>
2019-12-10 21:36:57 +0100

    Added analog stick servo control

--- main/main.c
+++ main/main.c
@@ -2,11 +2,75 @@
 #include "ps3.h"
 #include "nvs_flash.h"
 #include "driver/gpio.h"
+#include "driver/ledc.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
 #define STATUS_LED_GPIO 2
 #define START_LED_GPIO 5
+#define SERVO_PIN 13
+
+#define SERVO_MIN_PWM 600
+#define SERVO_MAX_PWM 2360
+#define SERVO_NEUTRAL_PWM 1500
+
+#define PS3_ANALOG_MIN -128
+#define PS3_ANALOG_MAX 127
+
+/////
+const int frequency = 50;  // Hz
+const int bit_resolution = 15;
+
+int get_duty(int pulse) {
+    double pulse_100_percent = 1e6 / frequency;
+    double duty_100_percent = 1 << bit_resolution;
+
+    double progress = ((double)pulse) / pulse_100_percent;
+    double duty = progress * duty_100_percent;
+
+    return (int)duty;
+}
+
+
+void set_up_servo(int channel, int pin) {
+    // configure channel
+    ledc_channel_config_t channel_config = {
+        .channel = channel,
+        .duty = get_duty(SERVO_NEUTRAL_PWM),
+        .gpio_num = pin,
+        .intr_type = LEDC_INTR_DISABLE,
+        .speed_mode = LEDC_HIGH_SPEED_MODE,
+        .timer_sel = LEDC_TIMER_0
+    };
+    ledc_channel_config(&channel_config);
+}
+
+
+void set_up_servos() {
+    // configure timer
+    ledc_timer_config_t timer_config = {
+        .duty_resolution = bit_resolution,
+        .freq_hz = frequency,
+        .speed_mode = LEDC_HIGH_SPEED_MODE,
+        .timer_num = LEDC_TIMER_0
+    };
+    ledc_timer_config(&timer_config);
+
+    // configure servos
+    set_up_servo(0, SERVO_PIN);
+}
+
+
+// Supply a value between 127 and -128
+void set_pulse(int channel, int8_t val) {
+    int ps3_range = PS3_ANALOG_MAX - PS3_ANALOG_MIN;
+    int servo_range = SERVO_MAX_PWM - SERVO_MIN_PWM;
+    int pulse = (((val - PS3_ANALOG_MIN) * servo_range) / ps3_range) + SERVO_MIN_PWM;
+
+    ledc_set_duty(LEDC_HIGH_SPEED_MODE, channel, get_duty(pulse));
+    ledc_update_duty(LEDC_HIGH_SPEED_MODE, channel);
+}
+/////
 
 bool isStarted = false;
 
@@ -15,6 +79,7 @@ void toggleStart(void) {
   gpio_set_level(START_LED_GPIO, isStarted ? 1 : 0);
 }
 
+int8_t prev = 0;
 void controller_event_cb(ps3_t ps3, ps3_event_t event) {
   if(event.button_up.start) {
     toggleStart();
@@ -25,19 +90,9 @@ void controller_event_cb(ps3_t ps3, ps3_event_t event) {
     return;
   }
 
-  if(event.analog_changed.stick.rx) {
-    printf("leftx %i", event.analog_changed.stick.lx);
+  if(ps3.analog.stick.lx) {
+    set_pulse(0, ps3.analog.stick.lx);
   }
-
-  /*
-    if ( event.button_down.cross ) {
-      gpio_set_level(START_LED_GPIO, 1);
-    }
-
-    if ( event.button_up.cross ) {
-      gpio_set_level(START_LED_GPIO, 0);
-    }
-    */
 }
 
 void init_gpio(void)
@@ -53,6 +108,8 @@ void app_main(void)
 {
   init_gpio();
 
+  set_up_servos();
+
   nvs_flash_init();
 
   uint8_t mac_addr[8] = {0};
@@ -67,7 +124,7 @@ void app_main(void)
     char macStr[19];
     snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
          mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5] + 2);
-    printf("Connect PS3 controller to master %s", macStr);
+    printf("Connect PS3 controller to master %s\n", macStr);
   }
 
   ps3SetEventCallback(controller_event_cb);
@@ -79,7 +136,6 @@ void app_main(void)
   while (!ps3IsConnected()){
       gpio_set_level(STATUS_LED_GPIO, blink ? 1 : 0);
       blink = !blink;
-      // Prevent the Task Watchdog from triggering
       vTaskDelay(100 / portTICK_PERIOD_MS);
   }
 
@@ -87,11 +143,8 @@ void app_main(void)
   // before pushing the LED configuration.
   vTaskDelay(250 / portTICK_PERIOD_MS);
   ps3SetLed(1);
+  vTaskDelay(250 / portTICK_PERIOD_MS);
   ps3Enable();
   gpio_set_level(STATUS_LED_GPIO, 1);
   printf("PS3 controller connected");
-
-  while (1) {
-    vTaskDelay(10 / portTICK_PERIOD_MS);
-  }
 }
